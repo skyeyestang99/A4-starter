6405a3cc1c200dc449b873cf0bcef5e808ed38df
skyeyestang99 <tommytang9958@gmail.com>
2022-02-07 18:21:16 -0800

    compile

--- operate.c
+++ operate.c
@@ -5,14 +5,7 @@
 #include "stack.h"
 #include "operate.h"
 
-/// operate on the instruction pointed by inst which is located at the given pc.  Return
-/// the new PC or -1 if there was an error
-///
-/// errors may include:
-///   insufficent operands on the stack
-///   unrecognized instruction.
-
-int operate(int pc, instr_t* inst) {
+int operate(int pc, instr_t* inst){
 
     switch (inst->opcode) {
         case PUSH:
